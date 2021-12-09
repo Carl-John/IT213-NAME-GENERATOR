5712eb158808779b45bbb7da78c209c3cff5c393
Carl-John <jy081320@gmail.com>
2021-12-09 13:11:34 +0800

    Project Finalization

--- js/app.js
+++ js/app.js
@@ -2,60 +2,50 @@ document.querySelector('#generate-names').addEventListener('submit', loadNames);
 
 
 
-
-//Execute the function to query the API
+// Execute the function to query the API
 function loadNames(e) {
     e.preventDefault();
 
 
-    //Read the values from the form and create the variables
+    // Read the values from the form and create the variables
     const origin = document.getElementById('country').value;
     const genre = document.getElementById('genre').value;
     const amount = document.getElementById('quantity').value;
 
-    //Build the URL
-    let url = 'http://uinames.com/api/?';
-    //Read the origin and append to the URL
+    // Build the URL
+    let url = 'https://randomuser.me/api/?';
+    //Read the origin and append to the url
     if(origin !== ''){
-        url += `?region=${origin}&`;
+        url += `nat=${origin}&`;
     }
-    //Read the genre and append to the URL
-    if(genre !== ''){
-        url += `?gender=${genre}&`;
+     //Read the genre and append to the url
+     if(genre !== ''){
+        url += `gender=${genre}&`;
     }
-    //Read the amount and append to the URL
+    //Read the amount and append to the url
     if(amount !== ''){
-        url += `?amount=${amount}&`;
+        url += `results=${amount}`;
     }
+    
+   //Get API
+   fetch(url)
+   .then(res => res.json())
+   .then(data => {
+        // console.log(data);
 
-    //Ajax Call
-    const xhr = new XMLHttpRequest();
+        let names = data.results;
+        
+        // Insert into the HTML
+        let html = "<h2><center>Generated Names</center></h2>"
+        html += '<ul class="list">';
+        names.forEach(function(name) {
+            html += `
+                <li>${name.name.first}</li>
+            `;     
+        })
+        html += '</ul>';
 
-    //Open the Connection
-    xhr.open('GET', url, true );
-
-    //Execute the function
-    xhr.onload = function() {
-        if(this.status === 200) {
-            const names = JSON.parse( this.responseText );
-            
-            //Insert into the HTML
-
-            let html = '<h2>Generated Names</2>';
-            html += '<ul names"list">';
-            names.forEach(function(name){
-                html +=`
-                    <li>${namae.name}</li>
-                `;
-            });
-            html += '</ul>';
-
-            document.querySelector('#result').innerHTML = html;
-
-        }
-    }
-
-    //Send the Request
-    xhr.send();
-
-}
+        document.querySelector('#result').innerHTML = html;
+   });
+           
+}  
